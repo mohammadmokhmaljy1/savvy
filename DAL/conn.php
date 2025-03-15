90a190c4439cb322b885841e7f35093353ff16d3
Mohammad Mokhmaljy <noorhhsas@gmail.com>
2025-03-15 21:44:49 +0300

    create to all basices files

---
+++ DAL/conn.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "prog_company_manage";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4";
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ];
+            $this->conn = new PDO($dsn, $this->username, $this->password, $options);
+
+        } catch (PDOException $e) {
+            die("خطأ في الاتصال: " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
